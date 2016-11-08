2ac275150c063337f941b71b2d1c448ed6047d15
Krzysztof Lech <krzysztof.lech@24abc.pl>
2016-11-08 07:57:52 +0100

    Utworzono szkielet aplikacji: - 2 vc

---
+++ formularz/FirstVC.swift
@@ -0,0 +1,21 @@
+//
+//  FirstVC.swift
+//  formularz
+//
+//  Created by Black Thunder on 08.11.2016.
+//  Copyright © 2016 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+class FirstVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+}
---
+++ formularz/FormVC.swift
@@ -0,0 +1,24 @@
+//
+//  FormVC.swift
+//  formularz
+//
+//  Created by Black Thunder on 08.11.2016.
+//  Copyright © 2016 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+class FormVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+    
+
+
+
+}
